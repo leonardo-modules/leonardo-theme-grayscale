ab7fc66ce07d116e017fa6ffeb957269056ad6b3
dresl <nikicresl@gmail.com>
2016-03-24 12:52:50 +0100

    add new grayscale theme

---
+++ leonardo_theme_grayscale/__init__.py
@@ -0,0 +1,13 @@
+
+LEONARDO_APPS = [
+    'leonardo_theme_grayscale',
+    'leonardo_module_analytics',
+]
+
+LEONARDO_JS_FILES = [
+    'js/tcpcloud.js',
+]
+
+LEONARDO_CSS_FILES = [
+    'css/animate.css',
+]
